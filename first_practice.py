f2a43fca15b544c9528cb071e03be38b82bad1dd
Raja Rautela <rajarautela393@gmail.com>
2026-03-09 21:17:05 +0530

    Add files via upload

---
+++ first_practice.py
@@ -0,0 +1,10 @@
+age = 20;
+height = 172;
+name = "raja";
+is_student = True
+print("my age is:", age);
+print("my height is:", height);
+print("my name is:", name);
+print("you are the student is class 12th:", is_student);
+print("you are the student is class 11th:", is_student);
+print("")
