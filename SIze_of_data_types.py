d382e6f41d8da38dadb00bd118860146a916f592
Raja Rautela <rajarautela393@gmail.com>
2026-03-09 21:31:04 +0530

    Add files via upload

---
+++ SIze_of_data_types.py
@@ -0,0 +1,14 @@
+name ="raja";
+age =20;
+weight =58.89;
+
+print("my name is:",name);
+print("my age is:",age);
+print("my weight is:",weight);
+
+print(type(name));
+print(type(age));
+print(type(weight));
+
+justification = "the above code is correct because we have used the correct syntax and data types for the variables name, age, and weight. The print statements are also correctly formatted to display the values of the variables and their types."
+print(justification);
