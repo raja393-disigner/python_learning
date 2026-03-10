7db76721f904e23c94cd1ece44025057213605ed
Raja Rautela <rajarautela393@gmail.com>
2026-03-10 20:11:11 +0530

    Add files via upload

---
+++ operater.py
@@ -0,0 +1,37 @@
+# logical operator
+# a =  50
+# b = 50
+
+# print(not False)
+# print(not (a>b))
+
+
+
+
+# val1 = True
+# val2 = True
+# print("And operetor:", val1 and val2)
+# print("Or operetor:", val1 or val2)
+# print("Or operetor:", (a==b) and  (a>=b))
+
+
+
+#relational operator
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+
+
+# Assign
+# ment opertor
+num = 10
+num **= 10
+
+print("num", num)
+
+
+
