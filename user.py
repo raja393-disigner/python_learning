a20fcd097d9d50560b13b144c0d7c9930ce2fd18
Raja Rautela <rajarautela393@gmail.com>
2026-03-15 22:27:58 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,15 @@
+# name = input("enter yout name: ")
+# age = input("enter yout age: ")
+# marks = input("enter yout marks" \
+# ": ")
+
+
+# print("my name is: ",name)
+# print("age is : ",age)
+# print("my marks: ",marks)
+
+
+a  = int(input("enter first number "))
+b  = int(input("enter second number "))
+
+print("two numbers sum", (a+b));
